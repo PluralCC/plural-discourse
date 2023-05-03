c469b2f2a0c94a40eadd3e48a7a8c08c9cff627e
democrazyfi <democrazyfi@gmail.com>
2023-05-03 19:55:27 +0400

    Fix fmt

--- app/models/users_allow_list.rb
+++ app/models/users_allow_list.rb
@@ -8,12 +8,12 @@ end
 #
 # Table name: users_allow_lists
 #
-#  id                :bigint           not null, primary key
-#  hashed_email      :string           not null
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
+#  id           :bigint           not null, primary key
+#  hashed_email :string          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 # Indexes
 #
-#  index_users_allow_lists_on_hashed_email (hashed_email) UNIQUE
+#  index_users_allow_lists_on_hashed_email  (hashed_email) UNIQUE
 #
